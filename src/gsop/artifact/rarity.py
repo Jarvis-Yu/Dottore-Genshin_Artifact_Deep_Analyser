3147c825ecaebb3515b26ccfd22ac501ea077aab
JarvisYu <JarvisYu_Pro@outlook.com>
2022-07-18 14:05:31 +0100

    implts rarity in terms of resin and domain runs

--- src/gsop/artifact/rarity.py
+++ src/gsop/artifact/rarity.py
@@ -1,6 +1,5 @@
 __fpath__ = "artifact"
 
-import time
 from typing import Dict, Set
 
 import numpy as np
@@ -13,6 +12,27 @@ from src.gsop.values.constants import P_3_SUBSTAT_ART_DOMAIN, \
 from src.gsop.values.terminology.artifact import ArtifactEnum as ae
 from src.gsop.values.terminology.attribute import AttributeEnum as attre
 
+FIVE_STAR_ARTIFACT_PER_CONDENSED = 2.141484
+CONDENSED_RESIN_VALUE = 40
+
+
+def rarity_expectation(p: float) -> float:
+    """
+    Returns 1/p
+    """
+    return 1 / p
+
+
+def rarity_in_resin(p: float) -> float:
+    return CONDENSED_RESIN_VALUE * rarity_expectation(p) / FIVE_STAR_ARTIFACT_PER_CONDENSED
+
+
+def rarity_in_domain_runs(p: float, use_resin=True) -> float:
+    if use_resin:
+        return rarity_expectation(p) / FIVE_STAR_ARTIFACT_PER_CONDENSED
+    else:
+        return rarity_expectation(p) / (FIVE_STAR_ARTIFACT_PER_CONDENSED / 2)
+
 
 def p_get_set(setName=None) -> float:
     return 0.5
@@ -23,7 +43,7 @@ def p_get_type(artifact_type: ae = None) -> float:
 
 
 def p_get_mainstat(artifact_type: ae, mainstat: attre) -> float:
-    weights = ae.mainstat_weights()
+    weights = artifact_type.mainstat_weights()
     return weights[mainstat] / wd_total_weight(weights)
 
 
@@ -40,6 +60,12 @@ def p_y_useful_given_x(artifact_type: ae, mainstat: attre,
                        usefulstats: Set[attre], x: int, y: int) -> float:
     """
     Given the artifact has x substats at level 0, there are y useful
+    :param artifact_type: type is ArtifactEnum
+    :param mainstat:  type is AttributeEnum
+    :param usefulstats:  type is Set(AttributeEnum)
+    :param x: number of substats at level 0
+    :param y: desired number of useful stats at level 0
+    :return: probability that the conditions are satisfied
     """
     func_called(__fpath__ + "p_y_useful_given_x")
 
@@ -84,7 +110,7 @@ def p_y_useful_given_x(artifact_type: ae, mainstat: attre,
         # print(sum(p_success) + sum(p_fail))
         return sum(p_success)
 
-    def dynamic_programming():
+    def dynamic_programming() -> float:
         possible_sub_stats: Dict = artifact_type.substat_weights().copy()
         possible_sub_stats.pop(mainstat, None)
         substats: Set = set()  # current substats
@@ -103,7 +129,9 @@ def p_y_useful_given_x(artifact_type: ae, mainstat: attre,
                 for attr in case:  # remove existing stats from pool
                     saved_data[attr] = possible_sub_stats.pop(attr)
                 for attr in possible_sub_stats:  # calc probability
-                    stats = set(case); stats.add(attr); frozen_stats = frozenset(stats)
+                    stats = set(case);
+                    stats.add(attr);
+                    frozen_stats = frozenset(stats)
                     now[frozen_stats] = now.setdefault(frozen_stats, 0) + base[case] * wd_p_key(
                         possible_sub_stats, attr)
                 for attr in case:  # restore values
@@ -127,19 +155,23 @@ def p_y_useful_given_x(artifact_type: ae, mainstat: attre,
         # print(sum(memorized_data[x].values()))
         return sum(p_success)
 
-    return brute_force()
-    # return dynamic_programming()
+    # return brute_force()
+    return dynamic_programming()
 
 
 if __name__ == '__main__':
-    start = time.time_ns()
-    for i in range(1000):
-        p_y_useful_given_x(ae.GOBLET, attre.HYDRO_DB, {attre.CRIT_RATE, attre.CRIT_DMG}, 3, 1)
-    end = time.time_ns()
-    print(end - start)
-    # print(p_y_useful_given_x(ae.GOBLET, attre.HYDRO_DB, {attre.CRIT_RATE, attre.CRIT_DMG}, 3, 1))
-    # a = {"a", "b"}
-    # b = {"b", "c"}
-    # c = {"b", "a"}
-    # d = {frozenset(a): 1, frozenset(b): 2}
-    # print(d.setdefault(frozenset(c), 100))
+    # start = time.time_ns()
+    # for i in range(1000):
+    #     p_y_useful_given_x(ae.GOBLET, attre.HYDRO_DB, {attre.CRIT_RATE, attre.CRIT_DMG}, 3, 1)
+    # end = time.time_ns()
+    # print(end - start)
+    useful_attrs = {attre.CRIT_RATE, attre.CRIT_DMG}
+    p_set = p_get_set()
+    p_goblet = p_get_type(ae.GOBLET)
+    p_mainstat = p_get_mainstat(ae.GOBLET, attre.PYRO_DB)
+    p_3substat_filter = p_y_useful_given_x(ae.GOBLET, attre.PYRO_DB, useful_attrs, 3, 2)
+    p_4substat_filter = p_y_useful_given_x(ae.GOBLET, attre.PYRO_DB, useful_attrs, 4, 2)
+    p = p_set * p_goblet * p_mainstat * (
+                p_get_x_substats(3) * p_3substat_filter + p_get_x_substats(4) * p_4substat_filter)
+    p1 = p_set * p_goblet * p_mainstat * p_4substat_filter
+    print(rarity_in_domain_runs(p1))
