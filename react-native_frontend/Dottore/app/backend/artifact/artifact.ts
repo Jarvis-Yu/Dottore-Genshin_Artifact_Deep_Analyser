69c423d2c22a7e941e313347aac2a8c83be5d471
JarvisYu <JarvisYu_Pro@outlook.com>
2022-10-05 14:45:34 +0100

    Trans artifacts to ts

--- react-native_frontend/Dottore/app/backend/artifact/artifact.ts
+++ react-native_frontend/Dottore/app/backend/artifact/artifact.ts
@@ -1,4 +1,5 @@
-import { Artifacts } from "../consts/artifact_consts";
+import { Artifacts, Artifacts_key } from "../consts/artifact_consts";
+import { Attributes_key } from "../consts/attribute_consts";
 import { wd_p_key } from "../helpers/random_weighted_dict_selector";
 import { ArtifactAttrs } from "./artifact_attrs";
 import {
@@ -25,12 +26,20 @@ export class Artifact {
    * @param {number} level
    * @param {number} exp
    * @param {Any} artifact_set
-   * @param {string} artifact_type ArtifactEnum
-   * @param {string} mainattr AttributeEnum
+   * @param {Artifacts_key} artifact_type ArtifactEnum
+   * @param {Attributes_key} mainattr AttributeEnum
    * @param {ArtifactAttrs} subattrs
    * @param {WeightedAttrs} weighted_subattrs
    */
-  constructor(level, exp, artifact_set, artifact_type, mainattr, subattrs, weighted_subattrs) {
+  constructor(
+    level: number,
+    exp: number,
+    artifact_set: any,
+    artifact_type: Artifacts_key,
+    mainattr: Attributes_key,
+    subattrs: ArtifactAttrs,
+    weighted_subattrs: WeightedAttrs
+  ) {
     this.#level = level;
     this.#exp = exp;
     this.#artifact_set = artifact_set;
@@ -41,15 +50,12 @@ export class Artifact {
   }
 
   /**
-   * @param {weightedattrs} weighted_subattrs
+   * @param {WeightedAttrs} weighted_subattrs
    * @param {boolean} crit_based
    * @returns {number}
    */
-  current_rating({ weighted_subattrs = undefined, crit_based = false }) {
-    if (!weighted_subattrs) {
-      weighted_subattrs = this.#weighted_subattrs;
-    }
-    rating = artifact_current_rating(this.#mainattr, this.#subattrs, weighted_subattrs);
+  current_rating({ weighted_subattrs = this.#weighted_subattrs, crit_based = false }) {
+    const rating: number = artifact_current_rating(this.#mainattr, this.#subattrs, weighted_subattrs);
     if (crit_based) {
       return default_rating_to_crit_based_rating(rating);
     } else {
@@ -62,11 +68,8 @@ export class Artifact {
    * @param {boolean} crit_based
    * @returns {number}
    */
-  expected_rating({ weighted_subattrs = undefined, crit_based = false }) {
-    if (!weighted_subattrs) {
-      weighted_subattrs = this.#weighted_subattrs;
-    }
-    rating = artifact_rating_expectation(
+  expected_rating({ weighted_subattrs = this.#weighted_subattrs, crit_based = false }) {
+    const rating = artifact_rating_expectation(
       this.#artifact_type,
       this.#mainattr,
       this.#level,
@@ -85,11 +88,8 @@ export class Artifact {
    * @param {boolean} crit_based
    * @returns {number}
    */
-  extreme_rating({ weighted_subattrs = undefined, crit_based = false }) {
-    if (!weighted_subattrs) {
-      weighted_subattrs = this.#weighted_subattrs;
-    }
-    rating = best_possible_rating(
+  extreme_rating({ weighted_subattrs = this.#weighted_subattrs, crit_based = false }) {
+    const rating = best_possible_rating(
       this.#artifact_type,
       this.#mainattr,
       this.#level,
@@ -107,10 +107,7 @@ export class Artifact {
    * @param {weightedattrs} weighted_subattrs
    * @returns {number}
    */
-  relative_p({ weighted_subattrs = undefined }) {
-    if (!weighted_subattrs) {
-      weighted_subattrs = this.#weighted_subattrs;
-    }
+  relative_p({ weighted_subattrs = this.#weighted_subattrs }) {
     const p_main_attr = wd_p_key(
       Artifacts[this.#artifact_type].mainattr_weights_readonly,
       this.#mainattr
@@ -133,17 +130,17 @@ export class Artifact {
 
   /**
    * @param {number} level
-   * @param {string} artifact_type  ArtifactEnum
-   * @param {string} mainattr AttributeEnum
+   * @param {Artifacts_key} artifact_type  ArtifactEnum
+   * @param {Attributes_key} mainattr AttributeEnum
    * @param {ArtifactAttrs} subattrs
    * @param {WeightedAttrs} [weights]
    * @returns
    */
   static rating_only_plan(
-    level,
-    artifact_type,
-    mainattr,
-    subattrs,
+    level: number,
+    artifact_type: Artifacts_key,
+    mainattr: Attributes_key,
+    subattrs: ArtifactAttrs,
     weights = WeightedAttrsPresets.crit_atk_plan
   ) {
     return new Artifact(level, 0, undefined, artifact_type, mainattr, subattrs, weights);
